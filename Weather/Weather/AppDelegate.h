3d1791e3f45062db24e07650a042bafb5d073b55
Bhanu Birani <bhanubirani@gmail.com>
2015-07-22 09:27:02 +0530

    initial project

---
+++ Weather/Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Bhanu Birani on 22/07/15.
+//  Copyright (c) 2015 Bhanu Birani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/Weather/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Weather
+//
+//  Created by Bhanu Birani on 22/07/15.
+//  Copyright (c) 2015 Bhanu Birani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
